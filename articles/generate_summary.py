03c3ee5b5d3e99a4265bcd0a3eb6e5c9302efb19
Kawa_ <yuyux0910@gmail.com>
2025-02-27 18:44:55 +0900

    feat: 記事の要約を生成して返す関数を作成

--- articles/generate_summary.py
+++ articles/generate_summary.py
@@ -1,7 +1,17 @@
+from google import genai
+from config import Config
 
+def content_to_summary(contents):
 
+    # Geminiのクライアントを作成する
+    client = genai.Client(api_key=Config.GEMINI_API_KEY)
 
+    prompt = '次の記事の本文を要約してください．'
 
+    response = client.models.generate_content(
+    model="gemini-2.0-flash",
+    contents=[prompt , contents]) #プロンプトと画像をgeminiに渡す
 
-def content_to_summary():
-    pass
+    response_summary = response.text
+
+    return response_summary
