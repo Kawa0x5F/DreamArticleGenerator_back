04c8154da0ca737ee885e28ef906b36d53666b56
funahashi0512 <stayfaao@gmail.com>
2025-02-27 20:34:30 +0900

    feat: add generate_content

--- articles/generate_content.py
+++ articles/generate_content.py
@@ -1,14 +1,36 @@
 from articles.database import get_chat_history
 from flask import Blueprint, request, jsonify
+from google import genai
+from config import Config
 
-def run_generate_content():
-    pass
+def run_generate_content(chat_history):
+    client = genai.Client(api_key=Config.GEMINI_API_KEY)
+    content_prompt = """
+    この会話から記事を生成してください。
+    ただし、記事にする部分は、夢に関する会話だけを利用するようにしてください。
+    """
+    response = client.models.generate_content(
+    model="gemini-2.0-flash",
+    contents=[content_prompt , chat_history]) #プロンプトと会話履歴をgeminiに渡す
+    response_content = response.text
+
+    client = genai.Client(api_key=Config.GEMINI_API_KEY)
+    title_prompt = """
+    この記事のタイトルを生成してください。
+    """
+    response = client.models.generate_content(
+    model="gemini-2.0-flash",
+    contents=[title_prompt , response_content]) #プロンプトと記事本体をgeminiに渡す
+    response_title = response.text
+
+    return  response_content, response_title
 
 
 def generated_article(chat_id):
     chat_history = get_chat_history(chat_id)
-    # title, content = run_generate_content(chat_history)
-    title, content = "aa", f"{chat_history}"
+    content, title = run_generate_content(chat_history)
+    # title = "aa"
+    # title, content = "aa", f"{chat_history}"
     response = {
         "id" : chat_id,
         "title" : title,
