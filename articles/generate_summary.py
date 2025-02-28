ff5da9ec8f080f16b53e1a0f6d573975addbbf3c
Kawa_ <yuyux0910@gmail.com>
2025-02-28 10:13:28 +0900

    fix: 要約の生成に条件を追加

--- articles/generate_summary.py
+++ articles/generate_summary.py
@@ -6,7 +6,13 @@ def content_to_summary(contents):
     # Geminiのクライアントを作成する
     client = genai.Client(api_key=Config.GEMINI_API_KEY)
 
-    prompt = '次の記事の本文を要約してください．'
+    prompt = """
+                次に自身の夢について書かれた記事を示します．
+                以下の条件に従ってその記事の内容を要約してください．
+                - 記事を書いた人の視点で要約する
+                - 要約は50文字程度の長さに収める
+                以上です．よろしくお願いします．
+             """
 
     response = client.models.generate_content(
     model="gemini-2.0-flash",
